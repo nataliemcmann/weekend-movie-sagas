348ed705c78256ae26969f7bdb9ecf6c8bc91494
Natalie McMann <nmcmann110@gmail.com>
2023-01-29 10:56:58 -0600

    added MUI to style movie list home page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,31 @@
 import {HashRouter as Router, Route} from 'react-router-dom';
 import './App.css';
+//app components
 import MovieList from '../MovieList/MovieList'
 import DetailsPage from '../DetailsPage/DetailsPage';
+//mui components
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Unstable_Grid2';
 
 function App() {
 
   return (
     <div className="App">
-      <h1>The Movies Saga!</h1>
+      <h1 className="App-header">Cool Movie List!</h1>
       <Router>        
         <Route path="/" exact>
-          <MovieList />
+          <Container maxWidth="xl">
+            <Grid 
+            container rowSpacing={4} 
+            columnSpacing={{ xs: 2 }}
+            >
+              <MovieList />
+            </Grid>
+          </Container>
         </Route>
-        <Route path="/:id" children={<DetailsPage />}>
-          
+        <Route 
+          path="/:id" 
+          children={<DetailsPage />}>
         </Route>
 
         {/* Add Movie page */}
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,11 +2,14 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import './MovieList.css'
+import { Card } from '@mui/material';
+import Grid from '@mui/material/Unstable_Grid2';
+
 
 function MovieList() {
     //declare dispatch
     const dispatch = useDispatch();
-    
+
     //declare history
     const history = useHistory();
 
@@ -32,18 +35,21 @@ function MovieList() {
 
     return (
         <main>
-            <h1>MovieList</h1>
             <section className="movies">
                 {movies.map(movie => {
                     return (
-                        <div key={movie.id} >
-                            <h3>{movie.title}</h3>
-                            <img 
-                                src={movie.poster} 
-                                alt={movie.title}
-                                onClick={(event) =>{sendToDetails(event, {movie})}}
-                                />
-                        </div>
+                        <Grid 
+                            card xs={2}
+                            key={movie.id}>
+                            <Card>
+                                <h3>{movie.title}</h3>
+                                <img 
+                                    src={movie.poster} 
+                                    alt={movie.title}
+                                    onClick={(event) =>{sendToDetails(event, {movie})}}
+                                    />
+                            </Card>
+                        </Grid>
                     );
                 })}
             </section>
