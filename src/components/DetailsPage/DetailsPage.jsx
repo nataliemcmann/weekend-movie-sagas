711133cce4619eb3d25867611f384d1157e56257
Natalie McMann <nmcmann110@gmail.com>
2023-01-29 11:56:34 -0600

    styled DetailsPage

--- src/components/DetailsPage/DetailsPage.jsx
+++ src/components/DetailsPage/DetailsPage.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+//import app components
 import GenreList from './GenreList';
 import BackButton from './BackButton';
+//mui components
+import { Box, Card, CardContent } from '@mui/material';
+import Grid from '@mui/material/Unstable_Grid2';
 
 function DetailsPage () {
         //declare dispatch
@@ -26,13 +30,33 @@ function DetailsPage () {
     return (
         <>
             <BackButton />
-            <h2>{movieDetails.title}</h2>
-            <img 
-                src={movieDetails.poster} 
-                alt={movieDetails.title}
-            /> 
-            <GenreList />
-            <p>{movieDetails.description}</p>
+            <Box sx={{p: 4}}> 
+                <Grid 
+                    flexDirection = "row"
+                    flexWrap = "nowrap"
+                    container spacing={2}
+                    justifyContent="center"
+                >
+                    <Card 
+                        sx={{ minWidth: 275}}>
+                        <CardContent>
+                            <h2>{movieDetails.title}</h2>
+                            <img 
+                                src={movieDetails.poster} 
+                                alt={movieDetails.title}
+                            /> 
+                        </CardContent>
+                    </Card>
+                    <Card 
+                        sx={{ maxWidth: 500}}
+                    >
+                        <CardContent>
+                            <GenreList />
+                            <p className="movieDesc">{movieDetails.description}</p>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </Box>
         </>
     )
 }
